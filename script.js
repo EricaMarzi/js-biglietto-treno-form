23db9a7a5b238e60da1a693f3c0a40e4be719af2
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-19 14:42:19 +0100

    add input

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("JS OK")
+
+/* 
+- Creo in pagina il form base 
+- Richiamo gli elementi necessari in JS assegnando una variabile
+- Recupero il valore dell'input
+*/
+
+const inputKms = document.getElementById("kms");
+const inputAge = document.getElementById("age");
+const button = document.getElementById("search");
+console.log(inputKms, inputAge, button)
+
+button.addEventListener("click", function() {
+    const kms = parseInt(inputKms.value);
+    const age = parseInt(inputAge.value);
+    console.log(kms, age)
+    
+
+})
